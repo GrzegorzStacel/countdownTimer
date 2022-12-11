b41994d9f4377356dc24dbdd8e1265c3e39c8675
Grzegorz Stącel <stacelgrzegorz@gmail.com>
2022-12-11 10:03:48 +0100

    Move context to components

--- src/App.js
+++ src/App.js
@@ -1,34 +1,9 @@
+import React from "react";
 import "./App.css";
-import React, { useState } from "react";
-import CountdownTimer from "./Components/atoms/CountdownTimer/CountdownTimer";
-import styled from "styled-components";
-import Calendar from "./Components/atoms/Calendar/Calendar";
-
-const AppContainer = styled.div`
-  margin: 0;
-  padding: 0;
-  background-color: gray;
-  width: 100vw;
-  height: 100vh;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-`;
-
-const StyledCalendar = styled(Calendar)`
-  --moedim-primary: green;
-`;
+import DateNote from "./Components/molecules/DateNote/DateNote";
 
 function App() {
-  const [value, setValue] = useState(new Date());
-  const setData = new Date(value).getTime();
-
-  return (
-    <AppContainer>
-      <h1>OC Peugeot</h1>
-      <Calendar funcGetDate={getDateFromCalendar}></Calendar>
-    </AppContainer>
-  );
+  return <DateNote heading={"OC/NW Peugeot"} />;
 }
 
 export default App;
