ba02c82700bdc0cdd6c3168ccdf53a5507f26cb1
Grzegorz Stącel <stacelgrzegorz@gmail.com>
2022-12-17 20:33:54 +0100

    Add PropTypes

--- src/Components/organisms/MainList.js
+++ src/Components/organisms/MainList.js
@@ -1,12 +1,14 @@
 import React from "react";
 import DateNote from "../molecules/DateNote/DateNote";
 import styled from "styled-components";
+import PropTypes from "prop-types";
 
 const Wrapper = styled.div`
   display: block;
   background: green;
   padding-top: 30px;
 `;
+
 const MainList = ({ dummyValues }) => {
   return (
     <Wrapper>
@@ -16,6 +18,15 @@ const MainList = ({ dummyValues }) => {
     </Wrapper>
   );
 };
+
+MainList.propTypes = {
+  dummyValues: PropTypes.arrayOf(
+    PropTypes.shape({
+      title: PropTypes.string.isRequired,
+      data: PropTypes.instanceOf(Date).isRequired,
+      id: PropTypes.number.isRequired,
+    })
+  ),
 };
 
 export default MainList;
