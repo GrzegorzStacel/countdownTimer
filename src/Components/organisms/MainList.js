aaefc6c8563c1974cb52e1dd0c9fbc4d3c4baee0
Grzegorz Stącel <stacelgrzegorz@gmail.com>
2022-12-17 20:32:29 +0100

    Add styled-coponents with styles

--- src/Components/organisms/MainList.js
+++ src/Components/organisms/MainList.js
@@ -1,8 +1,12 @@
 import React from "react";
 import DateNote from "../molecules/DateNote/DateNote";
+import styled from "styled-components";
 
-const MainList = () => {
-  return <DateNote heading={"OC/NW Peugeot"} />;
+const Wrapper = styled.div`
+  display: block;
+  background: green;
+  padding-top: 30px;
+`;
 };
 
 export default MainList;
