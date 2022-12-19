9a7dfbf52d7acdca234c27f9c4ad5d3ef6d8395e
Grzegorz Stącel <stacelgrzegorz@gmail.com>
2022-12-19 10:31:15 +0100

    Abbreviation of month names

--- src/App.js
+++ src/App.js
@@ -9,9 +9,9 @@ function App() {
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   const initialValues = [
-    { title: "OC Peugeot", data: new Date("December 20, 2023"), id: 1 },
-    { title: "Moje urodziny", data: new Date("September 25, 2023"), id: 2 },
-    { title: "Przegląd Vectra", data: new Date("January 13, 2023"), id: 3 },
+    { title: "OC Peugeot", data: new Date("Dec 20, 2024"), id: 1 },
+    { title: "Moje urodziny", data: new Date("Sept 25, 2023"), id: 2 },
+    { title: "Przegląd Vectra", data: new Date("Jan 13, 2023"), id: 3 },
   ];
 
   const sortedInitialValues = [].concat(initialValues);
