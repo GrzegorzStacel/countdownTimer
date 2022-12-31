df045b027782723c07253bf8a8ae7de99338c9e7
Grzegorz Stącel <stacelgrzegorz@gmail.com>
2022-12-31 13:27:42 +0100

    Beta version - Fetch data from db and show them in aplication instead of data from dummy values from app.js

--- src/Components/organisms/MainList.js
+++ src/Components/organisms/MainList.js
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import DateNote from "../molecules/DateNote/DateNote";
 import styled from "styled-components";
 import PropTypes from "prop-types";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../../firebase-config";
 
 const Wrapper = styled.div`
   display: block;
@@ -10,11 +12,42 @@ const Wrapper = styled.div`
 `;
 
 const MainList = ({ dummyValues }) => {
+  const [eventFromDB, setEventFromDB] = useState();
+
+  useEffect(() => {
+    console.log(eventFromDB);
+  }, [eventFromDB]);
+
+  useEffect(() => {
+    getEventFromDB();
+  }, []);
+
+  const getEventFromDB = () => {
+    const eventsCollectionRef = collection(db, "deadEnds");
+    getDocs(eventsCollectionRef)
+      .then((res) => {
+        const events = res.docs.map((doc) => ({
+          data: doc.data(),
+          id: doc.id,
+        }));
+        setEventFromDB(events);
+      })
+      .catch((err) => console.log(err.message));
+  };
+
   return (
     <Wrapper>
-      {dummyValues.map((e) => (
-        <DateNote heading={e.title} deadEndDate={e.data} key={e.id} />
-      ))}
+      {Array.isArray(eventFromDB)
+        ? eventFromDB.map((e) => (
+            <DateNote
+              heading={e.data.title}
+              deadEndDate={new Date(e.data.timeToEnd.seconds * 1000)}
+            />
+          ))
+        : console.log("null", eventFromDB)}
+      {/*  {dummyValues.map((e) => (*/}
+      {/*  <DateNote heading={e.title} deadEndDate={e.data} key={e.id} />*/}
+      {/*))}*/}
     </Wrapper>
   );
 };
