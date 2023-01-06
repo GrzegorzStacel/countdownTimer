31b2e15e1ec294c23c4a591ceba1635bd0043287
grzegorz <stacelgrzegorz@gmailcom>
2023-01-06 14:18:04 +0100

    Delete initialValues, function updateInitialValues and some props

--- src/App.js
+++ src/App.js
@@ -8,23 +8,6 @@ import plusIcon from "./assets/icons/Plusicon.svg";
 function App() {
   const [isModalOpen, setIsModalOpen] = useState(false);
 
-  const initialValues = [
-    { title: "OC Peugeot", data: new Date("Dec 20, 2024"), id: 1 },
-    { title: "Moje urodziny", data: new Date("Sept 25, 2023"), id: 2 },
-    { title: "Przegląd Vectra", data: new Date("Jan 13, 2023"), id: 3 },
-  ];
-
-  const sortValues = () => {
-    const sortedInitialValues = [].concat(initialValues);
-    sortedInitialValues
-      .sort((a, b) => (a.data.getTime() > b.data.getTime() ? 1 : -1))
-      .map((item, i) => <div key={i}> {item.data}</div>);
-
-    return sortedInitialValues;
-  };
-
-  const [stateValues, setStateValues] = useState(sortValues);
-
   const openModal = () => {
     setIsModalOpen(() => true);
   };
@@ -33,21 +16,12 @@ function App() {
     setIsModalOpen(() => false);
   };
 
-  const updateInitialValues = (newItem) => {
-    initialValues.push(newItem);
-    setStateValues(sortValues);
-  };
-
   return (
     <>
+      {console.log("App")}
       <ButtonIcon icon={plusIcon} onClick={openModal} />
-      {isModalOpen ? (
-        <AddNewInstance
-          closeModalFn={closeModal}
-          addNewItem={updateInitialValues}
-        />
-      ) : null}
-      <MainList openModalFn={openModal} dummyValues={stateValues} />
+      {isModalOpen ? <AddNewInstance closeModalFn={closeModal} /> : null}
+      <MainList openModalFn={openModal} />
     </>
   );
 }
