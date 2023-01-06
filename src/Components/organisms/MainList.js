fbc1889efff5340ef32ae1e69ebdb581987739f8
grzegorz <stacelgrzegorz@gmailcom>
2023-01-06 14:19:49 +0100

    Commit with not properly working code - this is only for save changes

--- src/Components/organisms/MainList.js
+++ src/Components/organisms/MainList.js
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from "react";
 import DateNote from "../molecules/DateNote/DateNote";
 import styled from "styled-components";
-import PropTypes from "prop-types";
-import { collection, getDocs } from "firebase/firestore";
+import { query, collection, orderBy, onSnapshot } from "firebase/firestore";
 import { db } from "../../firebase-config";
+import Loader from "react-loader-spinner";
 
 const Wrapper = styled.div`
   display: block;
@@ -12,54 +12,37 @@ const Wrapper = styled.div`
 `;
 
 const MainList = ({ dummyValues }) => {
-  const [eventFromDB, setEventFromDB] = useState();
+  const [eventFromDB, setEventFromDB] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    console.log(eventFromDB);
-  }, [eventFromDB]);
-
-  useEffect(() => {
-    getEventFromDB();
+    const fetchData = async () => {
+      const q = query(collection(db, "deadEnds"), orderBy("timeToEnd", "asc"));
+      const unsubscribe = onSnapshot(q, (querySnapShot) => {
+        let deadEndArr = [];
+        querySnapShot.forEach((doc) => {
+          deadEndArr.push({ ...doc.data(), id: doc.id });
+        });
+        setEventFromDB(deadEndArr);
+        setIsLoading(false);
+      });
+      return () => unsubscribe();
+    };
   }, []);
-
-  const getEventFromDB = () => {
-    const eventsCollectionRef = collection(db, "deadEnds");
-    getDocs(eventsCollectionRef)
-      .then((res) => {
-        const events = res.docs.map((doc) => ({
-          data: doc.data(),
-          id: doc.id,
-        }));
-        setEventFromDB(events);
-      })
-      .catch((err) => console.log(err.message));
-  };
 
   return (
     <Wrapper>
-      {Array.isArray(eventFromDB)
-        ? eventFromDB.map((e) => (
-            <DateNote
-              heading={e.data.title}
-              deadEndDate={new Date(e.data.timeToEnd.seconds * 1000)}
-            />
-          ))
-        : console.log("null", eventFromDB)}
-      {/*  {dummyValues.map((e) => (*/}
-      {/*  <DateNote heading={e.title} deadEndDate={e.data} key={e.id} />*/}
-      {/*))}*/}
+      {console.log("render")}
+      {!isLoading &&
+        eventFromDB.map((e) => (
+          <DateNote
+            key={e.id}
+            heading={e.title}
+            deadEndDate={new Date(e.timeToEnd.seconds * 1000)}
+          />
+        ))}
     </Wrapper>
   );
-};
-
-MainList.propTypes = {
-  dummyValues: PropTypes.arrayOf(
-    PropTypes.shape({
-      title: PropTypes.string.isRequired,
-      data: PropTypes.instanceOf(Date).isRequired,
-      id: PropTypes.number.isRequired,
-    })
-  ),
 };
 
 export default MainList;
