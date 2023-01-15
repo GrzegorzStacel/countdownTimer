d512f261bc03f9de6272ab526c78f30192a68d18
Grzegorz Stącel <stacelgrzegorz@gmail.com>
2023-01-15 14:42:17 +0100

    Refactoring - open/close modal change to the toggle modal

--- src/App.js
+++ src/App.js
@@ -6,21 +6,17 @@ import AddNewInstance from "./Components/molecules/AddNewInstance/AddNewInstance
 import plusIcon from "./assets/icons/PlusIcon.svg";
 
 function App() {
-  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [modalOpen, setModalOpen] = useState(false);
 
-  const openModal = () => {
-    setIsModalOpen(() => true);
-  };
-
-  const closeModal = () => {
-    setIsModalOpen(() => false);
+  const toggleModal = () => {
+    setModalOpen(!modalOpen);
   };
 
   return (
     <>
-      <ButtonIcon icon={plusIcon} onClick={openModal} />
-      {isModalOpen ? <AddNewInstance closeModalFn={closeModal} /> : null}
-      <MainList openModalFn={openModal} />
+      <ButtonIcon icon={plusIcon} onClick={toggleModal} />
+      {modalOpen ? <AddNewInstance closeModalFn={toggleModal} /> : null}
+      <MainList openModalFn={toggleModal} />
     </>
   );
 }
