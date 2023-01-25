a7e5c7b0b4bcd8c8edcf5540a53b076e4c71b93b
GrzegorzStacel <stacelgrzegorz@gmail.com>
2023-01-25 09:22:43 +0100

    Add comments

--- src/Components/atoms/Calendar/Calendar.js
+++ src/Components/atoms/Calendar/Calendar.js
@@ -7,19 +7,26 @@ const StyledCalendar = styled(Timetable)`
   --moedim-primary: green;
 `;
 
-const Calendar = ({ funcGetDate }) => {
-  const [deadLineDate, setDeadLineDate] = useState(new Date());
 const Calendar = ({
   handlerGetDateFromCalendar,
   fetchedDateStoredInTheDatabase,
 }) => {
+  // To set the starting date in the calendar we use one from two options:
+  // Adding a new entry - setting the current date: new Date() example: Sun Jan 22 2023 10:55:05 GMT+0100 (czas środkowoeuropejski standardowy)
+  // Editing an existing entry: editTimeSec example: Mon Jan 30 2023 00:00:00 GMT+0100 (czas środkowoeuropejski standardowy)
+
   let initialValues;
   if (fetchedDateStoredInTheDatabase) {
     initialValues = fetchedDateStoredInTheDatabase;
   } else {
     initialValues = new Date();
   }
+
+  // deadLineDate - Time taken from TimeTable from moedim (StyledCalendar).
+  // E.q output: Mon Jan 30 2023 00:00:00 GMT+0100 (czas środkowoeuropejski standardowy)
   const [deadLineDate, setDeadLineDate] = useState(initialValues);
+
+  // setDataInMs - e.q output: 1675033200000
   const setDataInMs = new Date(deadLineDate).getTime();
 
   useEffect(() => {
