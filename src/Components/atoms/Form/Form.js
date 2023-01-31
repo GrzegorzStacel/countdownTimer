ba794c8400e0930d6954deed162ca25194bbd4c4
GrzegorzStacel <stacelgrzegorz@gmail.com>
2023-01-31 19:48:43 +0100

    Move form code to new file and add changes that another components can use this component

---
+++ src/Components/atoms/Form/Form.js
@@ -0,0 +1,66 @@
+import React, { useEffect, useState } from "react";
+import DateLabel from "../DateLabel/DateLabel";
+import CountdownTimer from "../CountdownTimer/CountdownTimer";
+import Calendar from "../Calendar/Calendar";
+import { convertToTimestamp } from "../../../firebase/firebase-config";
+
+const Form = ({
+  handlerFormSubmit,
+  title = "",
+  handlerSetTitle,
+  maxLength,
+  handlerOnClose,
+  handlerTimeToEnd,
+  handlerSetDataFromCalendar,
+  submitNameButton,
+}) => {
+  const [titleForm, setTitleForm] = useState(title);
+  const [dataFromCalendar, setDataFromCalendar] = useState(
+    handlerTimeToEnd ? null : new Date().getTime()
+  );
+
+  useEffect(() => {
+    handlerSetDataFromCalendar(dataFromCalendar);
+  }, [dataFromCalendar]);
+
+  const getDateFromCalendar = (DateFromCalendar) => {
+    setDataFromCalendar(DateFromCalendar);
+  };
+
+  return (
+    <form onSubmit={handlerFormSubmit}>
+      <h1>Edytuj notatkę</h1>
+      <label htmlFor="title">Tytuł:</label>
+      <input
+        type="text"
+        id="title"
+        maxLength={maxLength}
+        defaultValue={title}
+        onFocus={(e) => e.target.select()}
+        onChange={(e) => {
+          handlerSetTitle(e.target.value);
+          setTitleForm(e.target.value);
+        }}
+        required
+      />
+      <p>
+        {titleForm.length}/{maxLength}
+      </p>
+      <DateLabel>{new Date(dataFromCalendar).toLocaleDateString()}</DateLabel>
+      <CountdownTimer countdownTimestampMs={dataFromCalendar} />
+      <Calendar
+        handlerGetDateFromCalendar={getDateFromCalendar}
+        fetchedDateStoredInTheDatabase={
+          handlerTimeToEnd ? convertToTimestamp(handlerTimeToEnd) : null
+        }
+      />
+
+      <button type="submit" value="Zaktualizuj">
+        {submitNameButton}
+      </button>
+      <button onClick={handlerOnClose}>Anuluj</button>
+    </form>
+  );
+};
+
+export default Form;
