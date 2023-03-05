18904fb6c173a7dbf4a885f68ed6dc8d30f326a0
GrzegorzStacel <stacelgrzegorz@gmail.com>
2023-03-05 21:51:34 +0100

    Add main styles

---
+++ src/theme/mainTheme.js
@@ -0,0 +1,27 @@
+export const theme = {
+  surface: "#393e46",
+  primary: "#03DAC6",
+  secondary: "#C38FFF",
+  on: {
+    surface: "#E1E1E1",
+    primary: "#00100E",
+    secondary: "#0C0911",
+  },
+  hover: {
+    surface: "#4D525B",
+    primary: "#41D9AE",
+    secondary: "#DCA0FA",
+  },
+  red: "#C03F3F",
+  green: "#44C961",
+  light: 300,
+  bold: 600,
+  fontSize: {
+    xxs: "1rem",
+    xs: "1.2rem",
+    s: "1.6rem",
+    m: "2.1rem",
+    l: "2.4rem",
+    xl: "4rem",
+  },
+};
