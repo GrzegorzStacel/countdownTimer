a69267909dc4493f01360a4881abcabeac93a018
GrzegorzStacel <stacelgrzegorz@gmail.com>
2023-03-05 21:54:00 +0100

    Remove logo and ButtonIcon from this component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import React, { useState } from "react";
 import "./App.css";
 import MainList from "./Components/organisms/MainList";
-import ButtonIcon from "./Components/atoms/ButtonIcon/ButtonIcon";
 import AddNewInstance from "./Components/molecules/AddNewInstance/AddNewInstance";
-import logo from "./assets/icons/logo.png";
-import plusIcon from "./assets/icons/PlusIcon.svg";
 import withInfoLabel from "./hoc/withInfoLabel";
 import GlobalStyle from "./theme/GlobalStyle";
 
@@ -22,8 +19,6 @@ function App({ manageShowingInfoLabel }) {
   return (
     <>
       <GlobalStyle />
-      <img src={logo} alt="Moja ikona" />
-      <ButtonIcon icon={plusIcon} onClick={toggleModal} />
       {isModalOpen ? (
         <AddNewInstance
           handlerToggleModal={toggleModal}
