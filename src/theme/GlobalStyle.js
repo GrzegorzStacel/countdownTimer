706927d976db419070c6780731275bf7698033fa
GrzegorzStacel <stacelgrzegorz@gmail.com>
2023-03-05 20:43:56 +0100

    Create global styles

---
+++ src/theme/GlobalStyle.js
@@ -0,0 +1,24 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+    @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@300;600&display=swap');
+    *, *::before, *::after {
+        box-sizing: border-box;
+        // Font Antialiasing https://devhints.io/css-antialias
+        // Fonty są faktycznie cieńsze niż gdybyśmy nie dodali tych dwóch linijek
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+    }
+    html {
+        font-size: 62.5%;
+    }
+    body {
+        font-size: 1.6rem;
+        font-family: "Montserrat", sans-serif;
+        background: #222831;
+        margin: 0;
+        padding: 0;
+    }
+`;
+
+export default GlobalStyle;
