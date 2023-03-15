f47fa78760bed675b8a7e17ee1a97de4357840b2
GrzegorzStacel <stacelgrzegorz@gmail.com>
2023-03-15 18:57:55 +0100

    Edit svg attributes

--- src/assets/icons/DeleteIcon.js
+++ src/assets/icons/DeleteIcon.js
@@ -4,10 +4,11 @@ const DeleteIcon = ({ theme }) => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      height="48"
-      width="48"
-      fill={theme.red}
-      stroke={theme.red}
+      viewBox="0 0 48 48"
+      width="33px"
+      height="33px"
+      fill={theme.on.surface}
+      stroke={theme.on.surface}
       aria-label="Ikona usnięcia"
     >
       <path d="M13.05 42q-1.25 0-2.125-.875T10.05 39V10.5H8v-3h9.4V6h13.2v1.5H40v3h-2.05V39q0 1.2-.9 2.1-.9.9-2.1.9Zm21.9-31.5h-21.9V39h21.9Zm-16.6 24.2h3V14.75h-3Zm8.3 0h3V14.75h-3Zm-13.6-24.2V39Z" />
