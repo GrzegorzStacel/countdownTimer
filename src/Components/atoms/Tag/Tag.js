931640ff2089e5f61a1d72ac7f43d7d85da30b13
GrzegorzStacel <stacelgrzegorz@gmail.com>
2023-04-02 11:14:11 +0200

    Add styles + selecting tag functionality

---
+++ src/Components/atoms/Tag/Tag.js
@@ -0,0 +1,53 @@
+import React, { useEffect, useState } from "react";
+import styled, { css } from "styled-components";
+import PropTypes from "prop-types";
+
+const Wrapper = styled.div`
+  width: 100px;
+  text-align: center;
+  padding: 5px 10px;
+  border: 2px solid transparent;
+  background-color: ${({ color }) => color};
+
+  &:hover {
+    filter: saturate(85%) brightness(85%);
+  }
+
+  ${({ isActive }) =>
+    isActive &&
+    css`
+      border-color: black;
+    `}
+`;
+const Tag = ({ color, children, isActive, id, handleSetSelectedTag }) => {
+  const [localIsActive, setLocalIsActive] = useState();
+
+  const handlerOnClick = () => {
+    handleSetSelectedTag(children, color, id);
+  };
+
+  useEffect(() => {
+    setLocalIsActive(isActive);
+  }, [isActive]);
+
+  return (
+    <Wrapper color={color} onClick={handlerOnClick} isActive={localIsActive}>
+      {children}
+    </Wrapper>
+  );
+};
+
+Tag.defaultProps = {
+  isActive: false,
+  color: "green",
+};
+
+Tag.propTypes = {
+  handleSetSelectedTag: PropTypes.func,
+  children: PropTypes.node.isRequired,
+  color: PropTypes.string.isRequired,
+  isActive: PropTypes.bool.isRequired,
+  id: PropTypes.string,
+};
+
+export default Tag;
